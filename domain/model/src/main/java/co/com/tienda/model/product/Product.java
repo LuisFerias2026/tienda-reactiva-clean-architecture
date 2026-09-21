68eacb3322cf0159f5933e5d2b8c15f8a3b98a8a
Luis Fernando Gomez Ospina <lfgospin@bancolombia.com.co>
2026-09-21 17:41:33 -0500

    fix: support product event deserialization

--- domain/model/src/main/java/co/com/tienda/model/product/Product.java
+++ domain/model/src/main/java/co/com/tienda/model/product/Product.java
@@ -2,13 +2,13 @@ package co.com.tienda.model.product;
 import lombok.Builder;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-//import lombok.NoArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import java.math.BigDecimal;
 
 @Getter
 @Setter
-//@NoArgsConstructor
+@NoArgsConstructor
 @AllArgsConstructor
 @Builder(toBuilder = true)
 public class Product {
